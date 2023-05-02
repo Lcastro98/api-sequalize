fca2afe90744514cc58200208d34b8ebff59d4da
Andrea Castro Manzano <andrea.castro@sofka.local>
2023-05-02 16:12:04 -0500

    Obteniendo datos de MySql con Sequelize

--- src/router/product.router.js
+++ src/router/product.router.js
@@ -3,12 +3,27 @@ const { faker } = require("@faker-js/faker");
 
 const Products = require("../model/product.model")
 
-router.get("/products", (req, res) => {
-    res.send("I am a Router")
+router.get("/products", async (req, res) => {
+    const products = await Products.findAll()
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: products
+    })
 });
 
-router.get("/products", (req, res) => {
-    res.send("I am a Router")
+router.get("/products/:product_id", async (req, res) => {
+    const id = req.params.product_id
+    const product = await Products.findOne({
+        where: {
+            product_id: id
+        }
+    })
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: product
+    })
 });
 
 router.post("/products", async (req, res) => {
