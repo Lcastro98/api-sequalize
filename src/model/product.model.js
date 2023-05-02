219a6e144ae74c05d03a617ab60d0e30b5b5fb53
Andrea Castro Manzano <andrea.castro@sofka.local>
2023-05-02 16:02:16 -0500

    Modelando tablas con Sequalize

--- src/model/product.model.js
+++ src/model/product.model.js
@@ -1,9 +1,35 @@
-const {Sequelize} = require("sequelize");
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("product_test", "root", "12345", {
     host: "localhost",
     dialect: "mysql"
 });
+
+class Product extends Model {}
+
+Product.init({
+    product_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.FLOAT(10, 2),
+        allowNull: false
+    },
+    is_stock: {
+        type: DataTypes.BOOLEAN
+    }
+}, {
+    sequelize,
+    modelName: "Product"
+});
+
+module.exports = Product;
 
 async function testConnection() {
     try {
